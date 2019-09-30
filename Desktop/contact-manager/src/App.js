0fdcf6bff8b90af43c5c4bb7960d05c7d14f0d58
Olaide <olaidemmanuel1@gmail.com>
2019-09-30 07:03:22 +0100

    I added edit contant component

--- Desktop/contact-manager/src/App.js
+++ Desktop/contact-manager/src/App.js
@@ -3,6 +3,7 @@ import Contacts from './components/contacts/Contacts';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import AddContact from './components/contacts/AddContact';
+import EditContact from './components/contacts/EditContact';
 import { Provider } from './context';
 import About from './components/Pages/About';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -25,6 +26,7 @@ class App extends React.Component {
                 <Switch>
                   <Route exact path="/" component={Contacts} />
                   <Route exact path="/contact/add" component={AddContact} />
+                  <Route exact path="/contact/edit/:id" component={EditContact} />
                   <Route exact path="/About" component={About} />
                   <Route exact path='/Test'  component={Test} />
                   <Route exact component={NotFound} />
--- Desktop/contact-manager/src/components/contacts/Contact.js
+++ Desktop/contact-manager/src/components/contacts/Contact.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 import { Consumer } from '../../context';
 import axios from 'axios';
+import { domainToASCII } from 'url';
 
 
  class Contact extends Component {
@@ -28,29 +30,36 @@ import axios from 'axios';
                             return (
                     <div className="card card-body mb-3">
                         <h4>{name}
-                        <i
-                        onClick={() =>
-                        this.setState({ showContactInfo: !this.state.showContactInfo })
-                        }
-                        className="fas fa-sort-down"
-                        style={{cursor:'pointer', color:'indigo'}}
-                        />
-                        <i className="fas fa-times"
-                            style={{ cursor:'pointer',
-                            float:'right',
-                            color:'blue' }}
-                              onClick={this.onDeleteClick.bind(this,id, dispatch)}
-
-
-                            // onClick={this.onDeleteClick.bind(this, id, dispatch)}
-                        />
+                                <i
+                                onClick={() =>
+                                this.setState({ showContactInfo: !this.state.showContactInfo })
+                                }
+                                className="fas fa-sort-down"
+                                style={{cursor:'pointer', color:'indigo'}}
+                                />
+                                <i className="fas fa-times"
+                                    style={{ cursor:'pointer',
+                                    float:'right',
+                                    color:'blue' }}
+                                    onClick={this.onDeleteClick.bind(this,id, dispatch)}
+                                />
+                                <Link to={ `contact/edit/${id}`}>
+                                    <i className="fas fa-pencil-alt"
+                                    style={{
+                                        cursor:"pointer",
+                                        float:"right",
+                                        color:"blue",
+                                        marginRight:"1rem"
+                                    }}
+                                    />
+                                </Link>
+                                {showContactInfo ?(<ul className="list-group">
+                                    <li className="list-group-item">
+                                    Email: {email}
+                                    </li>
+                                    <li className="list-group-item">Phone: {phone}</li>
+                                </ul>) : null}
                         </h4>
-                        {showContactInfo ?(<ul className="list-group">
-                            <li className="list-group-item">
-                            Email: {email}
-                            </li>
-                            <li className="list-group-item">Phone: {phone}</li>
-                        </ul>) : null}
                     </div>
                             )
                         }}
