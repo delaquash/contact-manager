c9659f007689417eff446563327f41e3c2d83ed7
Olaide <olaidemmanuel1@gmail.com>
2019-09-30 19:05:31 +0100

    Updates

---
+++ precache-manifest.61e23830097bf0871d99881aa26d7890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e045b1819b843bb4e8e4432522a81cb0",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "addf7937a425f7a591a3",
+    "url": "/contactmanager/static/css/2.b9dc5a1e.chunk.css"
+  },
+  {
+    "revision": "22f535750491a70df4ab",
+    "url": "/contactmanager/static/css/main.5a68ece1.chunk.css"
+  },
+  {
+    "revision": "addf7937a425f7a591a3",
+    "url": "/contactmanager/static/js/2.30781502.chunk.js"
+  },
+  {
+    "revision": "22f535750491a70df4ab",
+    "url": "/contactmanager/static/js/main.850c798e.chunk.js"
+  },
+  {
+    "revision": "c040dbd8586cd548482f",
+    "url": "/contactmanager/static/js/runtime~main.f3662a3d.js"
+  }
+]);
