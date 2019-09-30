8769447d1f6efbd72136f8c197e1d5b095450322
Olaide <olaidemmanuel1@gmail.com>
2019-09-30 17:45:38 +0100

    initial commit

--- Desktop/contact-manager/src/App.js
+++ Desktop/contact-manager/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Contacts from './components/contacts/Contacts';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Layout/Header';
 import AddContact from './components/contacts/AddContact';
 import EditContact from './components/contacts/EditContact';
